5dbb41bc2cdd58780ea1a68499913b6a01361d17
Innocent Ngene <ngeneinnocent@gmail.com>
2022-07-01 02:51:01 +0100

    login page

--- src/components/ActivityIndicator.js
+++ src/components/ActivityIndicator.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import LottieView from 'lottie-react-native';
-import colors from '../config/colors';
 
 function ActivityIndicator({ visible = false }) {
   if (!visible) return null;
