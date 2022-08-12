4a3170a5e606c88bb0537841da3bba696c910aae
Innocent Ngene <ngeneinnocent@gmail.com>
2022-08-12 15:31:21 +0100

    animate info modal

--- src/config/glossary.js
+++ src/config/glossary.js
@@ -1,6 +1,6 @@
 export default Object.freeze({
   CATEGORY_ICON_NAME: {
     header: 'Category icon name',
-    text: 'This icon will represent the category app wide. Input a Fontawesome icon name',
+    text: 'This icon will represent the category app wide. Input a Material design icon name',
   },
 });
