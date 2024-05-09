976250bfaaf185a839ccf928830b5ac3fbeeab02
Innocent Ngene <ngeneinnocent@gmail.com>
2024-05-09 20:12:35 +0100

    remove google service files

--- src/navigation/BaseUserNavigator.js
+++ src/navigation/BaseUserNavigator.js
@@ -71,50 +71,39 @@ function BaseUserNavigator() {
   }, []);
 
   return (
-    <>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          // REFACTOR!!!
-          tabBarIcon: ({ color, size }) => {
-            let iconName;
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        // REFACTOR!!!
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
 
-            if (route.name === constants.LOGBOOKS_TAB) {
-              iconName = 'list-outline';
-            } else if (route.name === constants.REWARDS_TAB) {
-              iconName = 'gift-outline';
-            } else if (route.name === constants.SETTINGS_TAB) {
-              iconName = 'settings-outline';
-            } else if (route.name === constants.PROFILE_TAB) {
-              iconName = 'person-outline';
-            }
+          if (route.name === constants.LOGBOOKS_TAB) {
+            iconName = 'list-outline';
+          } else if (route.name === constants.REWARDS_TAB) {
+            iconName = 'gift-outline';
+          } else if (route.name === constants.SETTINGS_TAB) {
+            iconName = 'settings-outline';
+          } else if (route.name === constants.PROFILE_TAB) {
+            iconName = 'person-outline';
+          }
 
-            return <Icon name={iconName} size={size} color={color} />;
-          },
-          tabBarStyle: {
-            height: 80,
-            paddingTop: 20,
-            paddingBottom: 20,
-          },
-          tabBarActiveTintColor: colors.primary,
-          tabBarInactiveTintColor: colors.lightGray,
-          headerShown: false,
-        })}
-      >
-        <Tab.Screen
-          name={constants.LOGBOOKS_TAB}
-          component={LogbooksNavigator}
-        />
-        <Tab.Screen name={constants.REWARDS_TAB} component={RewardsNavigator} />
-        <Tab.Screen
-          name={constants.PROFILE_TAB}
-          component={ProfilesNavigator}
-        />
-        <Tab.Screen
-          name={constants.SETTINGS_TAB}
-          component={SettingsNavigator}
-        />
-      </Tab.Navigator>
-    </>
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarStyle: {
+          height: 80,
+          paddingTop: 20,
+          paddingBottom: 20,
+        },
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.lightGray,
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name={constants.LOGBOOKS_TAB} component={LogbooksNavigator} />
+      <Tab.Screen name={constants.REWARDS_TAB} component={RewardsNavigator} />
+      <Tab.Screen name={constants.PROFILE_TAB} component={ProfilesNavigator} />
+      <Tab.Screen name={constants.SETTINGS_TAB} component={SettingsNavigator} />
+    </Tab.Navigator>
   );
 }
 
